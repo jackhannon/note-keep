27d6886abd7c3c293d5202c8a45fbb5538b758fd
Mezzichai <76605204+Mezzichai@users.noreply.github.com>
2024-03-05 12:31:01 -0800

    trying to get the env variables to work with npm script

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,17 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+const env = loadEnv(mode, process.cwd(), '');
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  test: {
-    environment: "jsdom",
-    setupFiles: "./tests/setup.ts"
-  }
+export default defineConfig(({ command, mode }) => {
+  return {
+    plugins: [react()],
+    test: {
+      environment: "jsdom",
+      setupFiles: "./tests/setup.ts"
+    },
+    define: {
+      'process.env.API_URL': JSON.stringify(env.API_URL || 'http://localhost:3000'),
+    },
+  };
 })
