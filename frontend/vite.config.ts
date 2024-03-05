692bb3d1a794846228f4db672e9ee8bbb1ce29cd
Mezzichai <76605204+Mezzichai@users.noreply.github.com>
2024-03-05 15:22:17 -0800

    reverted changes to environment variable accessing

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,17 +1,11 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-const env = loadEnv(mode, process.cwd(), '');
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command, mode }) => {
-  return {
-    plugins: [react()],
-    test: {
-      environment: "jsdom",
-      setupFiles: "./tests/setup.ts"
-    },
-    define: {
-      'process.env.API_URL': JSON.stringify(env.API_URL || 'http://localhost:3000'),
-    },
-  };
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    environment: "jsdom",
+    setupFiles: "./tests/setup.ts"
+  }
 })
