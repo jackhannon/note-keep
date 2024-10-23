f49d78aed619c5815f9d3f824f5c41684e9b0b46
Mezzichai <76605204+Mezzichai@users.noreply.github.com>
2024-10-22 21:25:05 -0700

    added deploy files

--- backend/config/allowedOrigins.ts
+++ backend/config/allowedOrigins.ts
@@ -2,7 +2,7 @@ const allowedOrigins = [
   'http://127.0.0.1:5500',
   'http://localhost:5173',
   'http://localhost:5000',
-  'https://keeperplus.up.railway.app' 
+  'https://jackhannon.github.io/note-keep/' 
 ];
 
 export {allowedOrigins}
