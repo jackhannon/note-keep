3517ee5ec7dab7c1ccdbd62955fbe510030901b6
Mezzichai <76605204+Mezzichai@users.noreply.github.com>
2024-10-23 00:08:10 -0700

    fix

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: "/note-keep/",
+    base: "/note-keep",
     test: {
       environment: "jsdom",
       setupFiles: "./tests/setup.ts",
