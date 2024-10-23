2554f18ea92b559dceca6ff4400b76a6441f1efc
Mezzichai <76605204+Mezzichai@users.noreply.github.com>
2024-10-22 23:07:57 -0700

    altered cors

--- backend/config/allowedOrigins.ts
+++ backend/config/allowedOrigins.ts
@@ -1,6 +1,9 @@
+import env from "./envConfig";
+
 const allowedOrigins = [
   'http://localhost:5173',
-  'https://jackhannon.github.io' 
+  'https://jackhannon.github.io',
+  `${env.CLIENT_DOMAIN}`, 
 ];
 
 export {allowedOrigins}
--- backend/config/corsOptions.ts
+++ backend/config/corsOptions.ts
@@ -1,11 +1,9 @@
 import { allowedOrigins } from "./allowedOrigins";
-import env from "./envConfig";
 
 const corsOptions = {
   origin: (origin: string | undefined, callback: (err: Error | null, allow?: boolean) => void) => {
     //short circuiting garauntees this will be a string if it gets to the 2nd half of the eval
-    if (origin === env.CLIENT_DOMAIN ||
-       (!origin || allowedOrigins.indexOf(origin) !== -1)) {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
       callback(null, true);
     } else {
       callback(new Error("Not allowed by CORS"));
